00d67ecf876cb721477d7306a9611ec04085d156
iswindle <isaac.swindle@gmail.com>
2021-09-16 16:53:44 -0500

    Added Fibonacci

--- src/com/techex/Main.java
+++ src/com/techex/Main.java
@@ -1,8 +1,15 @@
 package com.techex;
 
+import com.techex.coding.Question1;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println(Arrays.toString(Question1.fibo(20000)));
+
     }
 }
--- src/com/techex/coding/Question1.java
+++ src/com/techex/coding/Question1.java
@@ -3,4 +3,13 @@ package com.techex.coding;
 
 public class Question1 {
     // Calculate the Fibonacci Series Iteratively
+    // 0,1,1,2,3,5,8,13,21
+    // track last 2 digits
+    //if numbers greater than 1...
+    //Take in how long series run
+    //return full series[array]
+
+
+
+
 }
