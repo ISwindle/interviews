d459b1030d8ff42c979eeccba16bfda1a764a8cf
iswindle <isaac.swindle@gmail.com>
2021-09-16 16:53:53 -0500

    Changes by mr.hawkins

--- src/com/techex/coding/Question1.java
+++ src/com/techex/coding/Question1.java
@@ -2,6 +2,36 @@ package com.techex.coding;
 
 
 public class Question1 {
+    public static int [] fibo(int degrees){
+        int prev1 = 0;
+        int prev2 = 1;
+        int count =2;
+        int newNum;
+        int [] seriesArray = new int[degrees];
+        seriesArray[0]= prev1;
+        seriesArray[1] = prev2;
+        // TODO Cleanup < 3 degrees
+        if(degrees < 3){
+
+            if(degrees == 1){
+                seriesArray[0] = prev1;
+            }
+            if(degrees == 2){
+                seriesArray[0] = prev1;
+                seriesArray[1] = prev2;
+            }
+            return seriesArray;
+        }
+        for(int i = 2; i < degrees; i++){
+            newNum = prev1 +prev2;
+            seriesArray[i] = newNum;
+            prev1 = prev2;
+            prev2 = newNum;
+        }
+
+    return seriesArray;
+
+    }
     // Calculate the Fibonacci Series Iteratively
     // 0,1,1,2,3,5,8,13,21
     // track last 2 digits
