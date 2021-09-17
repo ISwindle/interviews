e932c7ca3c284db09e7ca6a646a31bba66b8b61d
iswindle <isaac.swindle@gmail.com>
2021-09-16 20:05:09 -0500

    Added Reverse String

--- src/com/techex/coding/Question2.java
+++ src/com/techex/coding/Question2.java
@@ -3,4 +3,19 @@ package com.techex.coding;
 // Return a Reversed String
 public class Question2 {
 
+    public static int[] reverseArray(int[] arrayToReverse) {
+
+        // arrayToReverse [1,2,3,4,5,6,7]
+        // returnReversed [7,0,0,0,0,0,0]
+        //for loop
+        var returnReversed = new int[arrayToReverse.length];
+        for(int i = arrayToReverse.length - 1; i >= 0; i--)
+        {
+            // arrayToReverse[4]
+            // returnReversed[7 - 4 - 1] = 2
+            returnReversed[arrayToReverse.length - i - 1] = arrayToReverse[i];
+        }
+
+        return returnReversed;
+    }
 }
